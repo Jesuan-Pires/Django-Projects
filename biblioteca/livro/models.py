b87cd68923d2ce7e147404d69a6fb0aa7ead1cf8
Jesuan <jdpires94@gmail.com>
2025-02-28 00:10:33 -0300

    criação da classe livro para modelagem do banco com ORM

--- biblioteca/livro/models.py
+++ biblioteca/livro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Livros(models.Model):
+    nome = models.CharField(max_length = 100)
+    autor = models.CharField(max_length = 30)
+    co_autor = models.CharField(max_length =  30, blank = True, null= True)
+    data_cadastro = models.DateTimeField(default = date.today)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length = 30, blank=True, null= True)
+    data_emprestimo = models.DateTimeField(blank=True, null= True)
+    data_devolucao = models.DateTimeField(blank=True, null= True)
+    tempo_duracao = models.TimeField(blank=True, null= True)
+
+    class Meta:
+        verbose_name = 'Livro'     
+
+     
